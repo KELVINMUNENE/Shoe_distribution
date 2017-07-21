c15d7f87fe55c372e69c394de697922c4a2ffdc5
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-21 13:01:29 +0300

    added shoulda matches to test our spec file using rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,15 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 require('./app')
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    with.library :active_record
+    with.library :active_model
+  end
+end
+
 
 
 
