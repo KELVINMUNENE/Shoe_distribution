6c392baeebad9ac4fb41149387afad2b78dd3048
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-21 17:20:07 +0300

    created methods to post brand and stores id

--- app.rb
+++ app.rb
@@ -56,3 +56,22 @@ get '/stores/:id' do
   @brands = Brand.all
   erb :store
 end
+
+
+
+post '/stores/:id/brand' do
+  store_id = params["store_id"].to_i
+  brand_id = params["brand_id"].to_i
+  @brand = Brand.find(brand_id)
+  @store = Store.find(store_id)
+  @store.brands.push(@brand)
+  redirect "/stores/#{@store.id}"
+end
+
+post '/stores/:id/brand/new' do
+  @store = Store.find(params["id"].to_i)
+  name = params["name"]
+  brand = Brand.create({name: name})
+  @store.brands.push(brand)
+  redirect "/stores/#{@store.id}"
+end
