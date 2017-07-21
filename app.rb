01ccf9fc1200557c724e859fe1983db97ab305a3
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-21 17:10:10 +0300

    created a variable which all are called in the index page

--- app.rb
+++ app.rb
@@ -2,3 +2,15 @@ require "bundler/setup"
 require "pry"
 Bundler.require :default
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+
+
+get '/' do
+  name = params['name']
+  @store = Store.create({name: name})
+  @brands = Brand.all
+  @stores = Store.all
+  @brands = Brand.all
+  erb :index
+end
+
