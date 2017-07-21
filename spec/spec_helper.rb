874bb77b5fe1af49ee34257d0dd70640e8fd993d
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-21 13:04:03 +0300

    configured rspec to test over brand and store classes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,16 @@ end
 
 
 
+RSpec.configure do |config|
+  config.after(:each) do
+    Brand.all().each() do |brand|
+      brand.destroy()
+    end
+    Store.all().each() do |store|
+      store.destroy()
+    end
+  end
+end
 
 
 
