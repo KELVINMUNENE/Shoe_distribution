e27ddd12c2711eb9d8c5a003cfa235ae8a33e05e
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-21 09:23:16 +0300

    created a table brands and added a column name

--- db/migrate/20170721052124_create_brands_stores.rb
+++
@@ -1,4 +0,0 @@
-class CreateBrandsStores < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- db/migrate/20170721052150_create_rename_table.rb
+++
@@ -1,5 +0,0 @@
-class RenameTable < ActiveRecord::Migration
-  def change
-    rename_table(:brand_store, :brands_stores)
-  end
-end
